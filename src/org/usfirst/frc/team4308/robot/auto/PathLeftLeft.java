f1cd75e1b91179d7bba41b8a732b54c24148d849
WilliamLQin <william.qin51@gmail.com>
2018-03-02 16:57:11 -0500

    add auto paths and create left and right paths for starting at center position

---
+++ src/org/usfirst/frc/team4308/robot/auto/PathLeftLeft.java
@@ -0,0 +1,11 @@
+package org.usfirst.frc.team4308.robot.auto;
+
+import edu.wpi.first.wpilibj.command.CommandGroup;
+
+public class PathLeftLeft extends CommandGroup {
+	
+	public PathLeftLeft() {
+		
+	}
+
+}
---
+++ src/org/usfirst/frc/team4308/robot/auto/PathRightLeft.java
@@ -0,0 +1,11 @@
+package org.usfirst.frc.team4308.robot.auto;
+
+import edu.wpi.first.wpilibj.command.CommandGroup;
+
+public class PathRightLeft extends CommandGroup {
+	
+	public PathRightLeft() {
+		
+	}
+
+}
