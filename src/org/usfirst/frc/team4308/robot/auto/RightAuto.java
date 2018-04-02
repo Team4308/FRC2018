5515fdfb82cb2fc2e870ab041caccbcd4e5b9b63
WilliamLQin <william.qin51@gmail.com>
2018-04-02 13:17:37 -0400

    right auto added 100%

--- src/org/usfirst/frc/team4308/robot/auto/RightAuto.java
+++ src/org/usfirst/frc/team4308/robot/auto/RightAuto.java
@@ -8,15 +8,31 @@ import edu.wpi.first.wpilibj.command.CommandGroup;
 public class RightAuto extends CommandGroup {
   
 	public RightAuto(String key){
+		
+		// Alliance switch is on the left
 		if (Robot.gameData.charAt(0) == ('L')) {
-			//Alliance Switch is on the left side; ignore and cross baseline
+			// 100% auto, go to other side to score cube
+			if (key.startsWith("100")) {
+				addSequential(new Move(-230.0)); // Move 230"
+				addSequential(new Rotate(-90.0)); // Rotate 90 degrees to the left
+				addSequential(new Move(-215.0)); // Move 215"
+				addSequential(new Rotate(-90.0)); // Rotate 90 degrees to the left
+				addSequential(new Move(-82.0));  // Move 82"
+				addSequential(new Rotate(-90.0)); // Rotate 90 degrees to the left
+				addSequential(new TimedMove(1.0, -1.0)); // For generous overshooting
+				addParallel(new DelayedCommand(new PullConveyor(false), 2.5), 0.5); // Move conveyor while moving, but a bit delayed
+			}
+			// No special setting, do not score cube
+			else {
+				addSequential(new Move(-148.0)); // Move 148"
+			}
+		} 
+		// Alliance switch is on the right
+		else {
 			addSequential(new Move(-148.0)); // Move 148"
-		} else {
-			//Alliance Switch is on the right side; go for switch
-			addSequential(new Move(-148.0)); // Move 148"
-			addSequential(new Rotate(-90.0));
-			addSequential(new Move(-20.0)); // Generous overshooting
-			addSequential(new PullConveyor(false), 2.5);
+			addSequential(new Rotate(-90.0)); // Rotate 90 degrees to the left
+			addSequential(new TimedMove(1.0, -1.0)); // For generous overshooting
+			addParallel(new DelayedCommand(new PullConveyor(false), 2.5), 0.5); // Move conveyor while moving, but a bit delayed
 		}
 
 	}
